892b984fa792767aaed07cce8cbb79741176d272
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2024-05-12 14:08:28 +0400

    plugged in scripts and styles

--- footer.php
+++ footer.php
@@ -1,35 +1,33 @@
-<footer class="footer">
-            <div class="container">
-                <div class="footer__inner">
-                    <div class="footer__logo">
-                        <img class="logo__img img-footer" src="./assets/icons/Raccoons-shop.svg" alt="Logo">
-                        <a class="logo logo-footer" href="#">
-                            <p class="logo__text">Raccoon's Shop</p>
-                        </a>
-                    </div>
-                    <div class="social">
-                        <!-- <a class="social__link" href="https://www.instagram.com/raccoons_shop_/" target="_blank">
-                            <img class="social__img" src="./assets/icons/instagram.svg" alt="Instagram">
-                        </a> -->
-                        <a class="social__link" href="tel:+79658455427" target="_blank">
-                            <img class="social__img" src="./assets/icons/phone-call.svg" alt="Phone">
-                        </a>
-                        <a class="social__link" href="mailto:masha2bogdanova@gmail.com" target="_blank">
-                            <img class="social__img" src="./assets/icons/mail.svg" alt="Mail">
-                        </a>
-                        <a class="social__link" href="https://t.me/cOmmon_rAccoon" target="_blank">
-                            <img class="social__img" src="./assets/icons/telegram.svg" alt="Telegram">
-                        </a>
-                    </div>
-                    <a class="footer__copyright" href="https://github.com/Iuliia-Bogdanova" target="_blank">© Iuliia Bogdanova, 2023</a>
+    <footer class="footer">
+        <div class="container">
+            <div class="footer__inner">
+                <div class="footer__logo">
+                    <img class="logo__img img-footer" src="./assets/icons/Raccoons-shop.svg" alt="Logo">
+                    <a class="logo logo-footer" href="#">
+                        <p class="logo__text">Raccoon's Shop</p>
+                    </a>
                 </div>
+                <div class="social">
+                    <!-- <a class="social__link" href="https://www.instagram.com/raccoons_shop_/" target="_blank">
+                        <img class="social__img" src="./assets/icons/instagram.svg" alt="Instagram">
+                    </a> -->
+                    <a class="social__link" href="tel:+79658455427" target="_blank">
+                        <img class="social__img" src="./assets/icons/phone-call.svg" alt="Phone">
+                    </a>
+                    <a class="social__link" href="mailto:masha2bogdanova@gmail.com" target="_blank">
+                        <img class="social__img" src="./assets/icons/mail.svg" alt="Mail">
+                    </a>
+                    <a class="social__link" href="https://t.me/cOmmon_rAccoon" target="_blank">
+                        <img class="social__img" src="./assets/icons/telegram.svg" alt="Telegram">
+                    </a>
+                </div>
+                <a class="footer__copyright" href="https://github.com/Iuliia-Bogdanova" target="_blank">© Iuliia Bogdanova, 2023</a>
             </div>
-        </footer>
+        </div>
+    </footer>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="./js/slick.min.js"></script>
-    <script src="./js/jquery.fancybox.min.js"></script>
-    <script src="./js/main.js"></script>
+    <?php wp_footer(); ?>
 
 </body>
+
 </html>  
